f6a089535a49513c8212609bfb7d52d037867db4
Cogeck <Dennis-schwarze@gmx.de>
2016-11-06 11:05:56 +0100

    JS Reload
    JS Reload

--- VapeForLife/HTML_Prebuilds/SmallHtmlPreBuild.cs
+++ VapeForLife/HTML_Prebuilds/SmallHtmlPreBuild.cs
@@ -9,10 +9,12 @@ namespace VapeForLife.HTML_Prebuilds
     public class SmallHtmlPreBuild
     {
         public string HTML { get; set; }
+        public string JS { get; set; }
 
         public SmallHtmlPreBuild()
         {
             this.HTML = "ReloadContent/AfterLogin/ReloadedAfterLogin.html";
+            this.JS = "ReloadContent/AfterLogin/ReloadedAfterLogin.js";
         }
     }
 }
