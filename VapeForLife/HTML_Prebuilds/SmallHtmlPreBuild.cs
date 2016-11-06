2d7a83039a5dbbdd98180adb4d4815e4ac12daac
JPOne <live@cyberkeks.net>
2016-11-06 13:17:19 +0100

    Added Packeges, Serverside

--- VapeForLife/HTML_Prebuilds/SmallHtmlPreBuild.cs
+++ VapeForLife/HTML_Prebuilds/SmallHtmlPreBuild.cs
@@ -5,16 +5,49 @@ using System.Web;
 
 namespace VapeForLife.HTML_Prebuilds
 {
-    //Masterklasse definieren!!!
+    /// <summary>
+    /// Definiert die nachzuladenen Inhalte
+    /// </summary>
     public class SmallHtmlPreBuild
     {
+        /// <summary>
+        /// HTML-Content der nachgeladen werden soll
+        /// </summary>
         public string HTML { get; set; }
+
+        /// <summary>
+        /// JS das nachgeladen werden soll
+        /// </summary>
         public string JS { get; set; }
 
+
+        /// <summary>
+        /// Konstruktor, setzt den HTML und JS auf string.Empty
+        /// </summary>
         public SmallHtmlPreBuild()
         {
-            this.HTML = "ReloadContent/AfterLogin/ReloadedAfterLogin.html";
-            this.JS = "ReloadContent/AfterLogin/ReloadedAfterLogin.js";
+            this.HTML = string.Empty;
+            this.JS = string.Empty;
+        }
+
+        /// <summary>
+        /// Konstruktor mit Übergabeparameter HTML der nachgeladen werden soll
+        /// </summary>
+        /// <param name="html">Der HTML der nachgeladen werden soll</param>
+        public SmallHtmlPreBuild(string html)
+        {
+            this.HTML = html;
+        }
+
+        /// <summary>
+        /// Konstruktor mit Übergabeparameter HTML und JS der nachgeladen werden soll
+        /// </summary>
+        /// <param name="html">Der HTML der nachgeladen werden soll</param>
+        /// <param name="JS">Das JS das nachgeladen werden soll</param>
+        public SmallHtmlPreBuild(string html, string JS)
+        {
+            this.HTML = html;
+            this.JS = JS;
         }
     }
 }
--- VapeForLife/Services/ServiceController.asmx.cs
+++ VapeForLife/Services/ServiceController.asmx.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Services;
 using System.Web.Script.Services;
 using System.Web.Script.Serialization;
+using VapeForLife.DBContext;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace VapeForLife.Services
 {
@@ -12,28 +16,91 @@ namespace VapeForLife.Services
     [ScriptService]
     public class ServiceController : WebService
     {
+        public static String sha256_hash(String value)
+        {
+            StringBuilder Sb = new StringBuilder();
+
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                Byte[] result = hash.ComputeHash(enc.GetBytes(value));
+
+                foreach (Byte b in result)
+                    Sb.Append(b.ToString("x2"));
+            }
+
+            return Sb.ToString();
+        }
+
         [WebMethod]
         [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
-        public HTML_Prebuilds.SmallHtmlPreBuild CheckIfUserAlloewdAccess(string user){
+        public HTML_Prebuilds.SmallHtmlPreBuild CheckIfUserAlloewdAccess(string user)
+        {
 
             var serializer = new JavaScriptSerializer(); //stop point set here
             DataClasses.User userObject = serializer.Deserialize<DataClasses.User>(user);
 
             HTML_Prebuilds.SmallHtmlPreBuild outPut;
 
-            //ToDo DBZugriff
-            if (userObject.Email == "TEST" && userObject.Password == "TEST")
+            if (!string.IsNullOrEmpty(userObject.Password))
             {
-                outPut = new HTML_Prebuilds.SmallHtmlPreBuild();
+
+                using (var entities = new VapeForLifeEntities())
+                {
+                    if (entities.users.Any())
+                    {
+                        var hash = sha256_hash(userObject.Password);
+                        
+                        if (entities.users.AsNoTracking().FirstOrDefault(e => e.Email.Equals(userObject.Email))?.Password?.Equals(hash) == true)
+                        {
+                            outPut = new HTML_Prebuilds.SmallHtmlPreBuild("reloadcontent/afterlogin/ReloadedAfterLogin.html");
+                        }
+                        else
+                        {
+                            outPut = new HTML_Prebuilds.SmallHtmlPreBuild();
+                            outPut.HTML = "NOPE";
+                        }
+                    }
+                    else
+                    {
+                        user u = new user();
+                        u.Name = "admin";
+                        u.Lastname = "admin";
+                        u.Nickname = "admin";
+                        u.Password = sha256_hash("admin");
+                        u.Email = "admin@admin.admin";
+                        entities.users.Add(u);
+                        entities.SaveChanges();
+                        outPut = new HTML_Prebuilds.SmallHtmlPreBuild();
+                        outPut.HTML = "NOPE";
+                    }
+                }
             }
             else
             {
                 outPut = new HTML_Prebuilds.SmallHtmlPreBuild();
-                //ToDo Besserenfall überlegen
                 outPut.HTML = "NOPE";
+
             }
-            
             return outPut;
-        }        
+        }
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public HTML_Prebuilds.SmallHtmlPreBuild SignUp()
+        {
+            HTML_Prebuilds.SmallHtmlPreBuild outPut = new HTML_Prebuilds.SmallHtmlPreBuild("reloadcontent/usermanagement/signup.html", "reloadcontent/usermanagement/register.js");
+
+            return outPut;
+        }
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public HTML_Prebuilds.SmallHtmlPreBuild Register(string nickname)
+        { 
+            HTML_Prebuilds.SmallHtmlPreBuild outPut = new HTML_Prebuilds.SmallHtmlPreBuild("reloadcontent/usermanagement/signup.html");
+
+            return outPut;
+        }
     }
 }
